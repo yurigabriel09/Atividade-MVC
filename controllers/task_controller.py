4499ebb0c25eca68a58f7237cb1d55eae6da8bc5
Henrique Santos Oliveira <2403384@SALAS.AULAS>
2025-09-01 22:24:13 -0300

    Adicionando task controller

---
+++ controllers/task_controller.py
@@ -0,0 +1,12 @@
+from flask import render_template
+from models.task import Task
+from config.database import db
+
+class TaskController:
+
+    @staticmethod
+    def list_tasks():
+        
+        tasks = db.session.query(Task).all()
+
+        return render_template('tasks.html', tasks=tasks)
